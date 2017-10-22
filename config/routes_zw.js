4ceeb83fae69862321f4f713c396d814685cbe47
zhangwei <123456>
2017-10-22 09:44:49 +0800

    提交创谷人才页面

--- config/routes_zw.js
+++ config/routes_zw.js
@@ -6,6 +6,10 @@ import Map from '../src/components/Map.vue'
 import InformationPara from '../src/components/InformationPara.vue'
 import Activitys from '../src/components/Activitys.vue'
 import ActivityPara from '../src/components/ActivityPara.vue'
+import Register from '../src/components/register/register.vue'
+import Login from '../src/components/register/login.vue'
+import ForgetPassword from '../src/components/register/forgetPassword.vue'
+import Talent from '../src/components/talent.vue'
 
 
 
@@ -18,9 +22,6 @@ import news from '../src/components/news.vue'
 import incubators from '../src/components/incubators.vue'
 import research from '../src/components/research.vue'
 import service from '../src/components/service.vue'
-import college from '../src/components/college.vue'
-import incubators_details from '../src/components/incubators_details.vue'
-
 
 
 
@@ -32,6 +33,12 @@ const routes = [
   {path: '/informationPara', component: InformationPara},
   {path: '/activitys', component: Activitys},
   {path: '/activityPara', component: ActivityPara},
+  {path: '/service', component: service},
+  {path: '/register', component: Register},
+  {path: '/login', component: Login},
+  {path: '/forgetPassword', component: ForgetPassword},
+  {path: '/talent', component: Talent},
+
 
 
 
@@ -42,9 +49,7 @@ const routes = [
 	{path: '/news', component: news},
 	{path: '/incubators', component: incubators},
 	{path: '/research', component: research},
-	{path: '/college', component: college},
-	{path: '/service', component: service},
-	{path: '/incubators_details', component: incubators_details}
+	{path: '/service', component: service}
 ]
 
 export default new VueRouter({
---
+++ src/components/talent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="talent">
+        <el-row>
+            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                <img src="../../static/img/talent_banner01.png" alt="">
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="24" :lg="24" style="background-color:#eeeeee;padding:20px 0;">
+                <div class="container">
+                    <el-form class="talent_form">
+                        <el-form-item>
+                            <el-col :xs="18" :sm="18" :md="18" :lg="18">
+                                <el-form-item>
+                                    <el-input placeholder="请输入职位关键词：如java工程师"></el-input>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :xs="6" :sm="6" :md="6" :lg="6">
+                                <el-button type="primary">搜索</el-button>
+                            </el-col>
+                        </el-form-item>
+                    </el-form>
+                </div>    
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                <div class="container">
+                    <div class="details_tag">
+
+                    </div>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
